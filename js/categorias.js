6d27244b52f4783bea3528437ac15335871bb7c2
aldu <aldana.cortez17@gmail.com>
2023-08-13 23:16:41 -0300

    ecommerce

---
+++ js/categorias.js
@@ -0,0 +1,23 @@
+// const categorias = [
+//     {
+//       id: 1,
+//       img: "../img/desarrollo.jpg",
+//       categoria: "Desarrollo",
+//       descripcion:
+//         "Nuestro equipo experto en desarrollo trabaja en colaboración vos para crear una página web que refleje la esencia de tu negocio",
+//     },
+//     {
+//       id: 2,
+//       img: "../img/marketing.jpg",
+//       categoria: "Marketing",
+//       descripcion:
+//         "Te ofrecemos estrategias efectivas y personalizadas para ayudarte a llegar a tus clientes potenciales y aumentar tus conversiones",
+//     },
+//     {
+//       id: 3,
+//       img: "../img/design.jpg",
+//       categoria: "Diseño",
+//       descripcion:
+//         "Desde las paletas hasta el diseño de la interfaz de usuario, nuestro objetivo es crear una página web atractiva y fácil que mejore tu presencia en línea",
+//     },
+//   ];
